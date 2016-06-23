bb714cfbe028ad5b3e7b91a3c7dedc833d0e8c93
Jeremy Bernard <jeremy.bernard@epitech.eu>
2016-06-23 12:09:05 +0200

    #31: revert new base

--- client/modules/daemon/index.js
+++
@@ -1,8 +0,0 @@
-const userInfo = require('../../config/base.config.json').userInfo;
-
-module.exports = (socket) => {
-  socket.on('getDaemonInfo', function(msg){
-    console.log('getDaemonInfo');
-    socket.emit('getDaemonInfo', {message: userInfo});
-  });
-}
