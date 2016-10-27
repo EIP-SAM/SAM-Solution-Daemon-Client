0b42b14153a2d1655f64990c1371ed3da88f3d8e
BernardJeremy <jeremy@bernard.sh>
2016-10-27 16:15:04 +0200

    #11: add notification display

---
+++ modules/notification/index.js
@@ -0,0 +1,8 @@
+const displayNotification = require('./displayNotification');
+
+module.exports = (socket) => {
+  /*
+    displayNotification :  Display notification
+   */
+   displayNotification(socket);
+}
