5aec5b27a4f02f1fd08ab59c75dac8f1a51fda0b
Nicolas Chauvin <chauvin.nico@gmail.com>
2016-11-24 16:37:09 +0100

    #22: Fix software package service calls

--- modules/software/exec.js
+++ modules/software/exec.js
@@ -10,13 +10,13 @@ commands.get_operating_system = packageManager.getOperatingSystem;
 
 function onSocketEvent(socket, method) {
   const fullMethodName = 'daemon_software_' + method;
-  socket.on(fullMethodName, function (params) {
-    console.log(method, params);
-    socket.emit(fullMethodName + '_status', { started: true });
+  socket.on(fullMethodName, function (params, commandIndex) {
+    console.log(method, params, commandIndex);
+    socket.emit(fullMethodName + '_status', { started: true, method: method, params: params });
     commands[method](params).then(function (returnStatus) {
-      socket.emit(fullMethodName + '_finished', returnStatus);
+      socket.emit(fullMethodName + '_finished_' + commandIndex, returnStatus);
     }).catch(function (error) {
-      socket.emit(fullMethodName + '_finished', { error });
+      socket.emit(fullMethodName + '_finished_' + commandIndex, { error });
     });
   });
 }
