268b1f4ecdfa88f67a100990386ba37a40f6b1a2
Nicolas Chauvin <chauvin.nico@gmail.com>
2017-01-12 15:26:47 +0100

    #33: Add windows service un/install scripts

---
+++ system_config/install_windows_service.js
@@ -0,0 +1,41 @@
+const Service = require('node-windows').Service;
+
+// Create a new service object
+const svc = new Service({
+  name:'SAM Solution Daemon Client',
+  description: 'System Administration Manager Solution: Localhost controller',
+  script: path.resolve(__diranme + '../client.js')
+});
+
+// Listen for the "install" event, which indicates the
+// process is available as a service.
+svc.on('install', () => {
+  console.log('install event');
+  svc.start();
+});
+
+svc.on('alreadyinstalled', () => {
+  console.log('alreadyinstalled event');
+});
+
+svc.on('invalidinstallation', () => {
+  console.log('invalidinstallation event');
+});
+
+svc.on('uninstall', () => {
+  console.log('uninstall event');
+});
+
+svc.on('start', () => {
+  console.log('start event');
+});
+
+svc.on('stop', () => {
+  console.log('stop event');
+});
+
+svc.on('error', () => {
+  console.log('error event');
+});
+
+svc.install();
---
+++ system_config/uninstall_windows_service.js
@@ -0,0 +1,38 @@
+const Service = require('node-windows').Service;
+
+// Create a new service object
+const svc = new Service({
+  name:'SAM Solution Daemon Client',
+  description: 'System Administration Manager Solution: Localhost controller',
+  script: path.resolve(__diranme + '../client.js')
+});
+
+svc.on('install', () => {
+  console.log('install event');
+});
+
+svc.on('alreadyinstalled', () => {
+  console.log('alreadyinstalled event');
+});
+
+svc.on('invalidinstallation', () => {
+  console.log('invalidinstallation event');
+});
+
+svc.on('uninstall', () => {
+  console.log('uninstall event');
+});
+
+svc.on('start', () => {
+  console.log('start event');
+});
+
+svc.on('stop', () => {
+  console.log('stop event');
+});
+
+svc.on('error', () => {
+  console.log('error event');
+});
+
+svc.uninstall();
